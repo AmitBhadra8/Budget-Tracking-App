800023fa46ecea105d23004a34e71e0de630184e
AmitBhadra <127890713+AmitBhadra8@users.noreply.github.com>
2024-12-02 00:07:19 +0530

    commiting budget tracking app

---
+++ lib/app_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyAppView extends StatelessWidget {
+  const MyAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Expense Tracker",
+      theme: ThemeData(
+        colorScheme: ColorScheme.light()
+      ),
+    );
+  }
+}
